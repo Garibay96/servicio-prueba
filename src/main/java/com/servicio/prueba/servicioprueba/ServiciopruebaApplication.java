dd911d7a98697bda4e382f40569550cc320bb5e2
Jhon Cajate <janthonyc1996@gmail.com>
2026-07-28 19:48:11 -0500

    Primer commit del servicio de prueba

---
+++ src/main/java/com/servicio/prueba/servicioprueba/ServiciopruebaApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.prueba.servicioprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciopruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciopruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servicio/prueba/servicioprueba/ServiciopruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.prueba.servicioprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciopruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
